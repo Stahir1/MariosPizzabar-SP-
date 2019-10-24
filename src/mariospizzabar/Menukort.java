628aee71bcaa5fa842f23cfadd6ccdf531feba72
danielbengtsen <cph-db145@cphbusiness.dk>
2019-10-24 16:04:27 +0200

    3 pizzaer mere

--- src/mariospizzabar/Menukort.java
+++ src/mariospizzabar/Menukort.java
@@ -28,24 +28,22 @@ public class Menukort {
         ArrayList<Pizza> marioMenu = new ArrayList();
         
         marioMenu.add(new Pizza(1, "Vesuvio", "tomatsauce, ost, skinke og oregano", 57));
-        //indsæt nyhed:
         marioMenu.add(new Pizza(2, "Amerikaner", "tomatsauce, ost, oksefars og oregano", 53));
         marioMenu.add(new Pizza(3, "Cacciatore", "tomatsauce, ost, pepperoni og oregano", 57));
-        //indsæt nyhed:
         marioMenu.add(new Pizza(4, "Carbona", "tomatsauce, ost, kødsauce, spaghetti, cocktailpølser og oregano", 63));
-        //indsæt nyhed:
         marioMenu.add(new Pizza(5, "Dennis", "tomatsauce, ost, skinke, pepperoni, cocktailpølser og oregano", 65));
         marioMenu.add(new Pizza(6, "Bertil", "tomatsauce, ost, bacon og oregano", 57));
         marioMenu.add(new Pizza(7, "Silvia", "tomatsauce, ost, pepperoni, rød peber, løg og oregano", 61));
         marioMenu.add(new Pizza(8, "Victoria", "tomatsauce, ost, skinke, ananas, champignon, løg og oregano", 61));
-        //indsæt nyhed:
         marioMenu.add(new Pizza(9, "Toronfo", "tomatsauce, ost, skinke, bacon, kebab, chili og oregano", 61));
         marioMenu.add(new Pizza(10, "Capricciosa", "tomatsauce, ost, skinke, champignon og oregano", 61));
         marioMenu.add(new Pizza(11, "Hawaii", "tomatsauce, ost, skinke, ananas og oregano", 61));
         marioMenu.add(new Pizza(12, "Le Blissola", "tomatsauce, ost, skinke, rejer og oregano", 61));
         marioMenu.add(new Pizza(13, "Venezia", "tomatsauce, ost, skinke, bacon og oregano", 61));
-        //indsæt nyhed:
         marioMenu.add(new Pizza(14, "Mafia", "tomatsauce, ost, pepperoni, bacon, løg og oregano", 61));
+        marioMenu.add(new Pizza(15, "Daniela", "tomatsauce, ost, kylling, bacon, løg, creme fraiche og oregano", 65));
+        marioMenu.add(new Pizza(16, "Mexicano", "tomatsauce, ost, kødsauce, chili, løg, majs og oregano", 65));
+        marioMenu.add(new Pizza(17, "Harry Potter", "tomatsauce, ost, kylling, bearnaise, hvidløg og oregano", 70));
         //TODO: lav op til 31 pizzaer.
         
         return marioMenu;
