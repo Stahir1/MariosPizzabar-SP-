aeed651c3c2689013adc9fcb054c835efac97406
“Stahir1” <sohaibapple@outlook.com>
2019-10-29 11:52:20 +0100

    Update Statistik.java

--- src/mariospizzabar/Statistik.java
+++ src/mariospizzabar/Statistik.java
@@ -1,9 +1,22 @@
 package mariospizzabar;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 /**
  *
- * @author Emil
+ * @author Sohaib
  */
 public class Statistik {
+   static String filename = "Data/MariosStatistik.csv";
+   public static void readFile() {
+   File file = new File(filename);
+   FileReader fr = new FileReader(file);
+   FileWriter fw =  new FileWriter(file);
+   
+   
+ 
+   }
     
 }
