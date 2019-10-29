81a2b08eb17ea830e732289afce3356e3397c5a6
danielbengtsen <cph-db145@cphbusiness.dk>
2019-10-29 12:02:11 +0100

    bestilling fjernet

--- src/mariospizzabar/Liste.java
+++ src/mariospizzabar/Liste.java
@@ -149,9 +149,14 @@ public class Liste {
         bw.close();
         fw.close();
         
-        oldFile.delete();
-        File dump = new File(filename);
-        newFile.renameTo(dump);
+        if(oldFile.delete()) {
+            File dump = new File(filename);
+            newFile.renameTo(dump);    
+            System.out.println("Bestilling fjernet.");
+        } else {
+            System.out.println("Bestilling kunne IKKE fjernes.");
+        }
+        
         
         
         }
