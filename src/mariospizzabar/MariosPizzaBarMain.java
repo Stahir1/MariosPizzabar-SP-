717ab0c689c43ecd059d3487a7daa2c1032b2431
VeraDev <laymasterlolz@gmail.com>
2019-11-11 18:17:22 +0100

    Rework if else statements til switches for at gøre det pænere og optimere designet på main

--- src/mariospizzabar/MariosPizzaBarMain.java
+++ src/mariospizzabar/MariosPizzaBarMain.java
@@ -13,83 +13,89 @@ public class MariosPizzaBarMain {
         Menukort marioMenukort = new Menukort();
         boolean runProg = true;
         int count = 1;
+        int choice = 0;
 
         System.out.println("Velkommen til Marios Pizzabar.");
         // runProg loopet håndtere afslutningen af programmet. 
         // Vi har en runProg boolean som er true som bliver ved med at køre programmet
         // indtil at runProg bliver false, hvorefter programmet lukkes. 
-        while (runProg) { 
+        
+        while (runProg) {
             System.out.println("Tast 1 for at se menukortet.");
             System.out.println("Tast 2 for at oprette en bestilling.");
             System.out.println("Tast 3 for at se bestillingslisten.");
             System.out.println("Tast 4 for at se omsætningen.");
             System.out.println("Tast 5 for at lukke programmet.");
-
             int number = IntScanner();
             String tidspunkt = "";
-            
-            /******************************************************************* 
-            * I menuen er der mulighed for at indtaste forskellige numre 
-            * som input og hvert nummer har en handling kodet. 
-            *******************************************************************/ 
 
-            if (number == 1) {
-                Menukort.getPizzaFromDB();
+            /**
+             * *****************************************************************
+             * I menuen er der mulighed for at indtaste forskellige numre som
+             * input og hvert nummer har en handling kodet.
+             * *****************************************************************
+             */
+            switch (number) {
+                case 1:
+                    Menukort.getPizzaFromDB();
+                    break;
+                case 2:
+                    System.out.println("\nLav en bestilling. \nIndtast Pizzanummer: ");
+                    number = IntScanner();
+                    System.out.println("Indtast afhentingstidspunkt: ");
+                    tidspunkt = StringScanner();
+                    System.out.println("\nBestilling gennemført:");
+                    Pizza tempPizza = marioMenukort.getPizzaByID(number);
+                    Bestilling bestilling = new Bestilling(number, tempPizza, tidspunkt);
+                    Liste marioListe = new Liste(bestilling);
+                    System.out.println(bestilling + "\n");
+                    marioListe.listeMaker(bestilling);
+                    Liste.writeFile(marioListe, "Data/Mariosliste.csv", count++);
+                    Liste.addPizzaToDB(bestilling);
+                    marioListe.removeBestilling(bestilling);
+                    break;
+                case 3:
+                    Liste.readFile("Data/Mariosliste.csv");
+                    System.out.println("Vil du fjerne en bestilling og markere den som ekspederet? (1 = JA, 2 = NEJ)");
+                    number = IntScanner();
+                    if (number == 1) {
+                        System.out.println("Hvilken bestilling vil du fjerne? (\"-n\")");
+                        String textIn = StringScanner();
+                        Liste.editFile2("Data/Mariosliste.csv", textIn, 1, ";");
+                    } else if (number == 2) {
+                    }
+                    break;
+                case 4:
+                    //Liste.readFile("Data/MariosEkspederet.csv"); // Mulighed for at læse MariosEkspederet.csv i programmet, hvis ønskes.
+                    Liste.fileToEksp("Data/MariosEkspederet.csv");
+                    System.out.println("\nVil du se listen over ekspederet ordre? (1 = JA, 2 = NEJ)");
+                    number = IntScanner();
+                    if (number == 1) {
+                        Liste.readFile("Data/MariosEkspederet.csv");
+                    } else if (number == 2) {
 
-            } else if (number == 2) {
-                System.out.println("\nLav en bestilling. \nIndtast Pizzanummer: ");
-                number = IntScanner();
-                System.out.println("Indtast afhentingstidspunkt: ");
-                tidspunkt = StringScanner();
-                System.out.println("\nBestilling gennemført:");
-                Pizza tempPizza = marioMenukort.getPizzaByID(number);
-                Bestilling bestilling = new Bestilling(number, tempPizza, tidspunkt);
-                Liste marioListe = new Liste(bestilling);
-                System.out.println(bestilling + "\n");
-                marioListe.listeMaker(bestilling);
-                Liste.writeFile(marioListe, "Data/Mariosliste.csv", count++);
-                Liste.addPizzaToDB(bestilling);
-                marioListe.removeBestilling(bestilling);
-            } else if (number == 3) {
-                Liste.readFile("Data/Mariosliste.csv");
-                System.out.println("Vil du fjerne en bestilling og markere den som ekspederet? (1 = JA, 2 = NEJ)");
+                    }
+                    break;
+                case 5: {
+                    runProg = false;
+                }
+
+               /* System.out.println("Vil du lukke programmet? (1 = JA, 2 = NEJ)");
                 number = IntScanner();
                 if (number == 1) {
-                    System.out.println("Hvilken bestilling vil du fjerne? (\"-n\")");
-                    String textIn = StringScanner();
-                    Liste.editFile2("Data/Mariosliste.csv", textIn, 1, ";");
+                    runProg = false;
                 } else if (number == 2) {
+                    runProg = true;
+                }*/ 
+                System.out.println("Programmet lukkes...");
+                break;
 
-                }
-
-            } else if (number == 4) {
-                //Liste.readFile("Data/MariosEkspederet.csv"); // Mulighed for at læse MariosEkspederet.csv i programmet, hvis ønskes.
-                Liste.fileToEksp("Data/MariosEkspederet.csv");
-                System.out.println("\nVil du se listen over ekspederet ordre? (1 = JA, 2 = NEJ)");
-                number = IntScanner();
-                if(number == 1) {
-                    Liste.readFile("Data/MariosEkspederet.csv");
-                } else if(number == 2) {
-                    
-                }
-            } else if (number == 5) {
-                runProg = false;
             }
-
-            System.out.println("Vil du lukke programmet? (1 = JA, 2 = NEJ)");
-            number = IntScanner();
-            if (number == 1) {
-                runProg = false;
-            } else if (number == 2) {
-                runProg = true;
-            }
-
         }
+        }
+        // Vi har to forskellige scannere. Én til Int som eksempelvis bruges til at nagivere rundt i menuen.
+        // Og én til String som bl.a. bruges til afhentningstidspunkt. 
 
-    }
-
-    // Vi har to forskellige scannere. Én til Int som eksempelvis bruges til at nagivere rundt i menuen.
-    // Og én til String som bl.a. bruges til afhentningstidspunkt. 
     public static int IntScanner() {
         Scanner myScan = new Scanner(System.in);
         int number = myScan.nextInt();
