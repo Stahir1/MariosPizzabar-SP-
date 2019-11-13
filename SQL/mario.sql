3195cdae92e495ab5ad85bee2ef9dc96af4165e1
VeraDev <laymasterlolz@gmail.com>
2019-11-13 11:44:25 +0100

    Tilføj SQL

--- SQL/mario.sql
+++ SQL/mario.sql
@@ -50,14 +50,46 @@ INSERT INTO `products` (`ProductID`, `ProductName`, `Topping`, `Price`) VALUES
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 -- Dumping structure for tabel shop2.orders
+-- --------------------------------------------------------
+-- Vært:                         127.0.0.1
+-- Server-version:               8.0.18 - MySQL Community Server - GPL
+-- ServerOS:                     Win64
+-- HeidiSQL Version:             10.1.0.5464
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for tabel mariopizza.orders
 DROP TABLE IF EXISTS `orders`;
 CREATE TABLE IF NOT EXISTS `orders` (
-  `OrderID` int(11) NOT NULL AUTO_INCREMENT,
+  `OrderID` int(11) NOT NULL DEFAULT '0',
+  `Ordering` int(11) NOT NULL DEFAULT '0',
   `Pizzaname` varchar(255) DEFAULT NULL,
   `Price` int(11) DEFAULT NULL,
   `PickupTime` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`OrderID`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`OrderID`,`Ordering`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table mariopizza.orders: ~3 rows (approximately)
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` (`OrderID`, `Ordering`, `Pizzaname`, `Price`, `PickupTime`) VALUES
+	(1, 1, 'Vesuvio', 57, '16:30'),
+	(1, 2, 'Dennis', 65, '16:30'),
+	(1, 3, 'Silvia', 61, '16:30'),
+	(1, 4, 'Toronfo', 61, '16:30'),
+	(2, 1, 'Hawaii', 61, '11:30'),
+	(2, 2, 'Danala', 61, '11:30'),
+	(3, 1, 'Mexicano', 65, '18:20');
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
 
 -- Dumping structure for tabel shop2.order_details
 DROP TABLE IF EXISTS `order_details`;
