525b505bde1a19bba7e1c2c7b790f98c253a713e
danielbengtsen <cph-db145@cphbusiness.dk>
2019-11-13 15:26:52 +0100

    Mariomenu SQL fil tilføjet

---
+++ SQL/marioMenu.sql
@@ -0,0 +1,38 @@
+/*
+-- Query: SELECT * FROM mariopizza.products
+LIMIT 0, 1000
+
+-- Date: 2019-11-13 15:26
+*/
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (1,'Vesuvio','tomatsauce, ost, skinke og oregano',57);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (2,'Amerikaner','tomatsauce, ost, oksefars og oregano',53);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (3,'Cacciatore','tomatsauce, ost, pepperoni og oregano',57);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (4,'Carbona','tomatsauce, ost, kødsauce, spaghetti, cocktailpølser og oregano',63);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (5,'Dennis','tomatsauce, ost, skinke, pepperoni, cocktailpølser og oregano',65);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (6,'Bertil','tomatsauce, ost, bacon og oregano',57);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (7,'Silvia','tomatsauce, ost, pepperoni, rød peber, løg og oregano',61);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (8,'Victoria','tomatsauce, ost, skinke, ananas, champignon, løg og oregano',61);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (9,'Toronfo','tomatsauce, ost, skinke, bacon, kebab, chili og oregano',61);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (10,'Capricciosa','tomatsauce, ost, skinke, champignon og oregano',61);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (11,'Hawaii','tomatsauce, ost, skinke, ananas og oregano',61);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (12,'Le Blissola','tomatsauce, ost, skinke, rejer og oregano',61);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (13,'Venezia','tomatsauce, ost, skinke, bacon og oregano',61);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (14,'Mafia','tomatsauce, ost, pepperoni, bacon, løg og oregano',61);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (15,'Danala','tomatsauce, ost, kylling, bacon, løg og oregano',61);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (16,'Mexicano','tomatsauce, ost, kødsauce, chili, løg, majs og oregano',65);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (17,'Harry Potter','tomatsauce, ost, kylling, bearnaise, hvidløg og oregano',70);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (18,'Terminator','tomatsauce, ost, kylling, creme fraiche , hvidløg og oregano',72);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (19,'Jackson','tomatsauce, ost, kylling, oksekød, paprika og oregano',64);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (20,'Voldamort','tomatsauce, ost, kylling, creme fraiche , hvidløg og chili',66);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (21,'Løkke','tomatsauce, ost, kylling, bearnaise, hvidløg og oregano',69);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (22,'DC','tomatsauce, ost, kylling, bearnaise, Kødsauce og oregano',65);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (23,'Marvel','tomatsauce, ost, kylling, bearnaise, hvidløg og creme fraiche',70);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (24,'Chicago','tomatsauce, ost, kylling, bearnaise, hvidløg og Kødsauce',70);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (25,'Bae','tomatsauce, ost, kylling, bearnaise, creme fraiche  og oregano',70);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (26,'Margrethe','tomatsauce, ost, kylling, bacon, hvidløg og oregano',70);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (27,'København','tomatsauce, ost, kylling, bearnaise, hvidløg og ananas',70);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (28,'Daniela','tomatsauce, ost, kylling, bacon, løg, creme fraiche og oregano',65);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (29,'SoHype','tomatsauce, ost, kylling, ananas, hvidløg, oksekød og oregano',70);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (30,'GitEmil','tomatsauce, ost, kylling, bearnaise, hvidløg og oregano',68);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (31,'Jimmay','tomatsauce, ost, kylling, bearnaise, creme fraiche og oregano',75);
+INSERT INTO `` (`ProductID`,`ProductName`,`Topping`,`Price`) VALUES (32,'Tullebob','tomatsauce, ost, kylling, bearnaise, hvidløg og oregano',80);
